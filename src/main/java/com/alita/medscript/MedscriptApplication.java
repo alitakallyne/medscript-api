89abb709dde7c6ac92b80017c8765d9bc17dbe66
alitakallyne <alytakallyne@gmail.com>
2026-04-16 08:24:07 -0300

    first commit

---
+++ src/main/java/com/alita/medscript/MedscriptApplication.java
@@ -0,0 +1,13 @@
+package com.alita.medscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedscriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedscriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alita/medscript/MedscriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alita.medscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedscriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
