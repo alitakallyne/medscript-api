601fe8d8ddf8c937a32a4223a2c844667ce76e0e
alitakallyne <alytakallyne@gmail.com>
2026-06-03 14:56:05 -0300

    test: configure test environment

--- src/test/java/com/alita/medscript/MedscriptApplicationTests.java
+++ src/test/java/com/alita/medscript/MedscriptApplicationTests.java
@@ -2,8 +2,10 @@ package com.alita.medscript;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MedscriptApplicationTests {
 
 	@Test
